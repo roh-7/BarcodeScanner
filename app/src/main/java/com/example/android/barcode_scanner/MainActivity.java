431957ad86e15491022d3abdca5b47c2c045d552
Danilo Lemes <daniloleemes@hotmail.com>
2017-10-14 10:10:42 -0300

    cleanup code and change ui

--- app/src/main/java/com/example/android/barcode_scanner/MainActivity.java
+++ app/src/main/java/com/example/android/barcode_scanner/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.android.barcode_scanner;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -17,44 +25,51 @@ import java.io.IOException;
 
 public class MainActivity extends Activity {
 
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private int CAMERA_REQUEST_CODE = 334;
+    private TextView barcodeInfo;
+    private BarcodeDetector barcodeDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final SurfaceView cameraView = (SurfaceView) findViewById(R.id.camera_view);
-        final TextView barcodeInfo = (TextView) findViewById(R.id.code_info);
-        BarcodeDetector barcodeDetector =
-                new BarcodeDetector.Builder(this)
-                        .setBarcodeFormats(Barcode.QR_CODE)
-                        .build();
 
-        final CameraSource cameraSource = new CameraSource
-                .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(640, 480)
+        setupDisplay();
+
+        cameraView.getHolder().addCallback(getCameraViewCallback());
+        barcodeDetector.setProcessor(getBarcodeProcessor());
+    }
+
+    private void setupDisplay() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        setupCamera(width, height);
+    }
+
+    private void setupCamera(int width, int height) {
+        cameraView = findViewById(R.id.camera_view);
+        barcodeInfo = findViewById(R.id.code_info);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
-        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    cameraSource.start(cameraView.getHolder());
 
-                } catch (IOException ie) {
-                    Log.e("CAMERA SOURCE", ie.getMessage());
-                }
-            }
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(width, height)
+                .build();
+    }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-
-        });
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+    @NonNull
+    private Detector.Processor<Barcode> getBarcodeProcessor() {
+        return new Detector.Processor<Barcode>() {
             @Override
             public void release() {
             }
@@ -73,6 +88,59 @@ public class MainActivity extends Activity {
                     });
                 }
             }
-        });
+        };
+    }
+
+    @NonNull
+    private SurfaceHolder.Callback getCameraViewCallback() {
+        return new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                            return;
+                        }
+                    }
+                    cameraSource.start(cameraView.getHolder());
+
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+
+        };
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                if (cameraSource != null) {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } else {
+                Toast.makeText(this, "Need camera permission", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
